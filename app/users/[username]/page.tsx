424dbec120fe047fdc966679fb581571d02fc5a3
Akmal <jasminaaa.20@uom.lk>
2025-07-10 09:23:31 +0530

    feat: create SingleUserPage component with breadcrumb navigation

---
+++ app/users/[username]/page.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { useParams } from "next/navigation";
+
+const SingleUserPage = () => {
+  const { username } = useParams();
+
+  return (
+    <div>
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/">Dashboard</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/components">Users</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>{username}</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+    </div>
+  );
+};
+
+export default SingleUserPage;
